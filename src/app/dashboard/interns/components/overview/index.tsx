f467a07c0c768f11444ec7001c5c14279ab13032
Snitrxm <andre.oliveira.roch@gmail.com>
2024-11-12 12:26:28 +0000

    feat: create intern dashboard

---
+++ src/app/dashboard/interns/components/overview/index.tsx
@@ -0,0 +1,86 @@
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { User } from "lucide-react"
+
+
+export const InternDashboardOverview = () => {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="grid grid-cols-4 gap-4">
+        {Array.from({ length: 4 }).map((item, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <div className="flex justify-between">
+                <h1 className="font-bold">Nota de FCT</h1>
+                <User />
+              </div>
+            </CardHeader>
+            <CardContent>
+              <h1 className="font-bold text-3xl">N/A</h1>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-2 gap-5">
+        <Card>
+          <div className="p-3">
+            <h1 className="text-xl">Registos de Presenças</h1>
+          </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Data</TableHead>
+                <TableHead>Manhã</TableHead>
+                <TableHead>Tarde</TableHead>
+                <TableHead>Tutor</TableHead>
+                <TableHead>Estado</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>12/12/2024</TableCell>
+                <TableCell>3</TableCell>
+                <TableCell>4</TableCell>
+                <TableCell>
+                  <Checkbox />
+                </TableCell>
+                <TableCell>Por Aprovar</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+        <Card>
+          <div className="p-3">
+            <h1 className="text-xl">Registos de Presenças</h1>
+          </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Data</TableHead>
+                <TableHead>Manhã</TableHead>
+                <TableHead>Tarde</TableHead>
+                <TableHead>Tutor</TableHead>
+                <TableHead>Estado</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              <TableRow>
+                <TableCell>12/12/2024</TableCell>
+                <TableCell>3</TableCell>
+                <TableCell>4</TableCell>
+                <TableCell>
+                  <Checkbox />
+                </TableCell>
+                <TableCell>Por Aprovar</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/dashboard/interns/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { InternDashboardOverview } from "./components/overview";
+import { InternDashboardAttendences } from "./components/attendences";
+import { InternDashboardWeeklySummaries } from "./components/weeklySummaries";
+
+export default function Dashboard() {
+    return (
+        <div className="h-screen w-screen">
+            <div className="flex justify-between items-center px-5 py-3 border-b-[1px] border-b-gray-300">
+                <h1 className="font-bold text-2xl">O seu dashboard</h1>
+                <Avatar>
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div className="px-4 py-3 flex flex-col gap-4">
+                <Tabs defaultValue="overview" className="w-full space-y-5">
+                    <TabsList className="grid w-[400px] grid-cols-4">
+                        <TabsTrigger value="overview">Overview</TabsTrigger>
+                        <TabsTrigger value="attendences">Presenças</TabsTrigger>
+                        <TabsTrigger value="weeklySummaries">Registos</TabsTrigger>
+                        <TabsTrigger value="avaliacoes">Avaliações</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="overview">
+                        <InternDashboardOverview />
+                    </TabsContent>
+                    <TabsContent value="attendences">
+                        <InternDashboardAttendences />
+                    </TabsContent>
+                    <TabsContent value="weeklySummaries">
+                        <InternDashboardWeeklySummaries />
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </div>
+    );
+}
