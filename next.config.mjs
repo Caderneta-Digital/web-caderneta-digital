d8dbd6aabba3bd94ae3b34608c652973e70b17a5
Snitrxm <andre.oliveira.roch@hotmail.com>
2025-01-24 22:08:45 +0000

    feat: change ip

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*", // Captura chamadas feitas para /api/*
-        destination: "http://100.28.23.135:8888/:path*", // Redireciona para o backend de prod
+        destination: "http://16.171.208.190:8888/:path*", // Redireciona para o backend de prod
         // destination: "http://localhost:8888/:path*", // Redireciona para o backend local
       },
     ];
