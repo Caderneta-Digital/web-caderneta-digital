9e037d19b92564876d6a530dcae37f0c87a2d030
Groimi <groimijoao@gmail.com>
2025-05-14 11:40:18 +0100

    abavv

--- src/app/dashboard/internInfo/details/[id]/(sub-pages)/grades/components/12ano/index.tsx
+++ src/app/dashboard/internInfo/details/[id]/(sub-pages)/grades/components/12ano/index.tsx
@@ -51,180 +51,174 @@ export const InternAdvisorDashboardGrades12Ano = () => {
       </div>
     );
   }
+
   const isIntern = user?.type === UserTypeEnum.INTERN;
-  //const isSupervisor = user?.type === UserTypeEnum.SUPERVISOR
   const isInternAdvisor = user?.type === UserTypeEnum.INTERN_ADVISOR;
 
   return (
     <div className="grid grid-cols-1 gap-6">
-          <Card>
-            <CardHeader className="flex flex-row justify-between items-center">
-              <CardTitle>Autoavaliação do Aluno</CardTitle>
-              <InternAdvisorGrades12anoInfo />
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Domínios/Ponderações</TableHead>
-                    <TableHead>Processos de Recolha de Informação</TableHead>
-                    <TableHead>Critérios de Avaliação</TableHead>
-                    <TableHead>Classificação (0-200)</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className="font-medium">
-                      Trabalho Prático (80%)
-                    </TableCell>
-                    <TableCell>Processo de Trabalho na FCT</TableCell>
-                    <TableCell>Participação</TableCell>
-                    <TableCell>{data ? data.participacao : "N/A"}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Autonomia</TableCell>
-                    <TableCell>{data ? data.autonomia : "N/A"}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Responsabilidade</TableCell>
-                    <TableCell>{data ? data.responsabilidade : "N/A"}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Relacionamento</TableCell>
-                    <TableCell>{data ? data.relacionamento : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell className="font-medium">
-                      Compreensão / Aplicação (20%)
-                    </TableCell>
-                    <TableCell>Relatório de FCT</TableCell>
-                    <TableCell>Pertinência</TableCell>
-                    <TableCell>{data ? data.pertinencia : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Rigor</TableCell>
-                    <TableCell>{data ? data.rigor : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Estruturação</TableCell>
-                    <TableCell>{data ? data.estruturacao : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Reflexão</TableCell>
-                    <TableCell>{data ? data.reflexao : "N/A"}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-              <div className="flex flex-row justify-between">
-                <div className="mt-4 text-sm text-neutral-950">
-                  Avaliação Final: {data ? data.finalGrade : "N/A"} &nbsp;&nbsp;&nbsp; Data: {data ? format(parseISO(data.createdAt), "dd/MM/yyyy") : "N/A"}
-                </div>
-                {isIntern && (
-                  <InternAdvisorGrades12anoAuto isButtonDisabled={data != "" as any} />
-                )}
-              </div>
-            </CardContent>
-          </Card>
-    
-          <Card>
-            <CardHeader className="flex flex-row justify-between items-center">
-              <CardTitle>Avaliação Final do Tutor</CardTitle>
-              <InternAdvisorGrades12anoInfo />
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Domínios/Ponderações</TableHead>
-                    <TableHead>Processos de Recolha de Informação</TableHead>
-                    <TableHead>Critérios de Avaliação</TableHead>
-                    <TableHead>Classificação (0-200)</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell className="font-medium">
-                      Trabalho Prático (80%)
-                    </TableCell>
-                    <TableCell>Processo de Trabalho na FCT</TableCell>
-                    <TableCell>Participação</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.participacao : "N/A"}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Autonomia</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.autonomia : "N/A"}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Responsabilidade</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.responsabilidade : "N/A"}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Relacionamento</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.relacionamento : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell className="font-medium">
-                      Compreensão / Aplicação (20%)
-                    </TableCell>
-                    <TableCell>Relatório de FCT</TableCell>
-                    <TableCell>Pertinência</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.pertinencia : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Rigor</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.rigor : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Estruturação</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.estruturacao : "N/A"}</TableCell>
-                  </TableRow>
-    
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell>Reflexão</TableCell>
-                    <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.reflexao : "N/A"}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-              <div className="mt-4 flex justify-between items-center">
-                <span className="text-sm text-neutral-950">
-                  Avaliação Final: {internAdvisorEvaluation ? internAdvisorEvaluation.finalGrade : "N/A"} &nbsp;&nbsp;&nbsp; Data: {internAdvisorEvaluation ? format(parseISO(internAdvisorEvaluation.createdAt), "dd/MM/yyyy") : "N/A"}
-                </span>
-                {
-                  isInternAdvisor && (
-                    <InternAdvisorGrades12anoFinal />
-                  )
-                : "N/A"}
+      <Card>
+        <CardHeader className="flex flex-row justify-between items-center">
+          <CardTitle>Autoavaliação do Aluno</CardTitle>
+          <InternAdvisorGrades12anoInfo />
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Domínios/Ponderações</TableHead>
+                <TableHead>Processos de Recolha de Informação</TableHead>
+                <TableHead>Critérios de Avaliação</TableHead>
+                <TableHead>Classificação (0-200)</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">
+                  Trabalho Prático (80%)
+                </TableCell>
+                <TableCell>Processo de Trabalho na FCT</TableCell>
+                <TableCell>Participação</TableCell>
+                <TableCell>{data ? data.participacao : "N/A"}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Autonomia</TableCell>
+                <TableCell>{data ? data.autonomia : "N/A"}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Responsabilidade</TableCell>
+                <TableCell>{data ? data.responsabilidade : "N/A"}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Relacionamento</TableCell>
+                <TableCell>{data ? data.relacionamento : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="font-medium">
+                  Compreensão / Aplicação (20%)
+                </TableCell>
+                <TableCell>Relatório de FCT</TableCell>
+                <TableCell>Pertinência</TableCell>
+                <TableCell>{data ? data.pertinencia : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Rigor</TableCell>
+                <TableCell>{data ? data.rigor : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Estruturação</TableCell>
+                <TableCell>{data ? data.estruturacao : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Reflexão</TableCell>
+                <TableCell>{data ? data.reflexao : "N/A"}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <div className="flex flex-row justify-between">
+            <div className="mt-4 text-sm text-neutral-950">
+              Avaliação Final: {data ? data.finalGrade : "N/A"} &nbsp;&nbsp;&nbsp; Data: {data ? format(parseISO(data.createdAt), "dd/MM/yyyy") : "N/A"}
+            </div>
+            {isIntern && (
+              <InternAdvisorGrades12anoAuto isButtonDisabled={data != "" as any} />
+            )}
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader className="flex flex-row justify-between items-center">
+          <CardTitle>Avaliação Final do Tutor</CardTitle>
+          <InternAdvisorGrades12anoInfo />
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Domínios/Ponderações</TableHead>
+                <TableHead>Processos de Recolha de Informação</TableHead>
+                <TableHead>Critérios de Avaliação</TableHead>
+                <TableHead>Classificação (0-200)</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">
+                  Trabalho Prático (80%)
+                </TableCell>
+                <TableCell>Processo de Trabalho na FCT</TableCell>
+                <TableCell>Participação</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.participacao : "N/A"}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Autonomia</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.autonomia : "N/A"}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Responsabilidade</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.responsabilidade : "N/A"}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Relacionamento</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.relacionamento : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell className="font-medium">
+                  Compreensão / Aplicação (20%)
+                </TableCell>
+                <TableCell>Relatório de FCT</TableCell>
+                <TableCell>Pertinência</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.pertinencia : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Rigor</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.rigor : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Estruturação</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.estruturacao : "N/A"}</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell>Reflexão</TableCell>
+                <TableCell>{internAdvisorEvaluation ? internAdvisorEvaluation.reflexao : "N/A"}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          <div className="mt-4 flex justify-between items-center">
+            <span className="text-sm text-neutral-950">
+              Avaliação Final: {internAdvisorEvaluation ? internAdvisorEvaluation.finalGrade : "N/A"} &nbsp;&nbsp;&nbsp; Data: {internAdvisorEvaluation ? format(parseISO(internAdvisorEvaluation.createdAt), "dd/MM/yyyy") : "N/A"}
             </span>
             {isInternAdvisor && <InternAdvisorGrades12anoFinal />}
           </div>
